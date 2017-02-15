a2c1f35e1e5545038324c5a10b76826c262d9cb4
Scott Morrison <scott.morrison@anu.edu.au>
2017-02-15 21:01:53 +1100

    Considering defining the Drinfeld centre

---
+++ drinfeld_centre.lean
@@ -0,0 +1,28 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Stephen Morgan, Scott Morrison
+import .category
+import .functor
+import .natural_transformation
+import .products
+import .monoidal_category
+
+--set_option pp.universes true
+
+open tqft.categories
+open tqft.categories.functor
+open tqft.categories.products
+open tqft.categories.natural_transformation
+open tqft.categories.monoidal_category
+
+namespace tqft.categories.drinfeld_centre
+
+-- structure HalfBraiding ( C : MonoidalCategory ) :=
+--     (object        : C^.Obj)
+--     (half_braiding : NaturalIsomorphism (tensor_on_left object) (tensor_on_right object))
+
+-- definition DrinfeldCentre ( C : MonoidalCategory ) : BraidedMonoidalCategory := {
+--
+-- }
+
+end tqft.categories.drinfeld_centre
