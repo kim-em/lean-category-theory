67facc4ddbf82e0962f577a76e135b90663cfc4e
Scott Morrison <scott.morrison@anu.edu.au>
2017-03-14 20:32:20 +1100

    commiting to record a segfault

--- tactics.lean
+++ tactics.lean
@@ -47,7 +47,11 @@ namespace tactic.interactive
      gs' ← get_goals,
      guard (gs ≠ gs') <|> tactic.fail "force tactic failed"
 
-  meta def trace_dunfold ( n : name ) : tactic unit := seq (force (dunfold (n :: list.nil) [])) (trace ("unfolding " ++ to_string n)) 
+  meta def trace_dunfold ( n : name ) : tactic unit := seq (force (dunfold [n] [])) (trace ("unfolding " ++ to_string n)) 
+
+  meta def unfold_at_least_one_with_attribute' (attr : parse ident) : tactic unit :=
+  do defs ← attribute.get_instances attr,
+     list.foldl orelse (tactic.fail "") (list.map trace_dunfold defs)
 
   meta def unfold_at_least_one_with_attribute (attr : parse ident) : tactic unit :=
   do defs ← attribute.get_instances attr,
@@ -56,7 +60,7 @@ end tactic.interactive
 
 attribute [unfoldable] cast
 
-meta def unfold_something (and_then : tactic unit) : tactic unit := try ( seq (tactic.interactive.unfold_at_least_one_with_attribute `unfoldable) and_then )
+meta def unfold_something (and_then : tactic unit) : tactic unit := try ( seq (tactic.interactive.unfold_at_least_one_with_attribute' `unfoldable) and_then )
 
 meta def blast  : tactic unit := smt_eblast >> pointwise blast >> unfold_something blast
 
