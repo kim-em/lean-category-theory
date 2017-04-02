4486310eb7c6a850df188ae648c4cb76492a7afb
Scott Morrison <scott.morrison@anu.edu.au>
2017-04-02 11:38:56 +1000

    tidying

--- examples/categories/cartesian_product.lean
+++ examples/categories/cartesian_product.lean
@@ -141,4 +141,22 @@ universe variables u v
   triangle := ♯
 }
 
+@[unfoldable] definition SymmetryOnCartesianProductOfCategories : Symmetry CartesianProductOfCategories := {
+  braiding             := {
+    morphism  := {
+      components := λ p, SwitchProductCategory p.1 p.2,
+      naturality := ♮
+    },
+    inverse   := {
+      components := λ p, SwitchProductCategory p.2 p.1,
+      naturality := ♮
+    },
+    witness_1 := ♯,
+    witness_2 := ♯
+  },
+  hexagon_1 := ♯,
+  hexagon_2 := ♯,
+  symmetry  := ♯
+}
+
 end tqft.categories.monoidal_category
