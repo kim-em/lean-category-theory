d724b7d6d6b1563d281d47b4df4bb658e6d37b43
Scott Morrison <scott.morrison@anu.edu.au>
2017-04-08 20:28:37 +1000

    making a huge mess; commiting a copy in case I need to recover something later. About to trash some junk!

--- monoidal_categories/lemmas/symmetry_in_terms_of_natural_transformations.lean
+++ monoidal_categories/lemmas/symmetry_in_terms_of_natural_transformations.lean
@@ -31,15 +31,64 @@ by cases Ha; cases Hb; reflexivity
 
 set_option trace.check true
 
+meta def rwonce : tactic unit :=
+do r ← tactic.to_expr `(FunctorComposition.associativity),
+   a ← tactic.to_expr `(associator),
+   tactic.rewrite_at_core reducible tt tt (occurrences.pos [2]) tt r a
+
+meta def rwonce_2 : tactic unit :=
+do r ← tactic.to_expr `(switch_twice_is_the_identity),
+   a ← tactic.to_expr `(switch_twice),
+   tactic.rewrite_at_core reducible tt tt (occurrences.pos [1]) tt r a
+
+meta def rwonce_3 : tactic unit :=
+do r ← tactic.to_expr `(FunctorComposition.left_identity),
+   a ← tactic.to_expr `(identitor),
+   tactic.rewrite_at_core reducible tt tt (occurrences.pos [2]) ff r a
+
 @[reducible] definition {u v} squared_Braiding { C : Category.{u v} } { m : MonoidalStructure C } ( commutor : Commutor m )
   : NaturalTransformation m.tensor m.tensor :=
   begin
-    pose square := vertical_composition_of_NaturalTransformations commutor.morphism (whisker_on_left (SwitchProductCategory C C) commutor.morphism),
-    refine ( cast _ square ),
-    -- refine ( transport _ square ),
-    rewrite - FunctorComposition.associativity,
-    erewrite switch_twice_is_the_identity,
-    rewrite FunctorComposition.left_identity,
+    pose second_commutor :=  whisker_on_left (SwitchProductCategory C C) commutor.morphism,
+    pose associator := IdentityNaturalTransformation (FunctorComposition (SwitchProductCategory C C) (FunctorComposition (SwitchProductCategory C C) (m.tensor))),
+    rwonce,
+    pose switch_twice := IdentityNaturalTransformation (IdentityFunctor (C × C)),
+    rwonce_2,
+    pose identitor := IdentityNaturalTransformation (FunctorComposition (IdentityFunctor (C × C)) (m.tensor)),
+    rwonce_3,
+    exact
+      vertical_composition_of_NaturalTransformations
+        commutor.morphism 
+        (vertical_composition_of_NaturalTransformations
+          second_commutor
+          (vertical_composition_of_NaturalTransformations
+            associator
+            (vertical_composition_of_NaturalTransformations
+              (whisker_on_right switch_twice m.tensor)
+              identitor))),
+
+    -- rewrite - FunctorComposition.associativity at second_commutor,
+    -- rewrite switch_twice_is_the_identity at second_commutor,
+    -- rewrite FunctorComposition.left_identity at second_commutor,
+    -- exact vertical_composition_of_NaturalTransformations commutor.morphism second_commutor,
+
+    -- pose second_commutor :=  whisker_on_left (SwitchProductCategory C C) commutor.morphism,
+    --     rewrite - FunctorComposition.associativity at second_commutor,
+    -- rewrite switch_twice_is_the_identity at second_commutor,
+    -- rewrite FunctorComposition.left_identity at second_commutor,
+    -- exact vertical_composition_of_NaturalTransformations commutor.morphism second_commutor,
+
+    -- pose square := vertical_composition_of_NaturalTransformations commutor.morphism (whisker_on_left (SwitchProductCategory C C) commutor.morphism),
+    -- rewrite - FunctorComposition.associativity at square,
+    -- rewrite switch_twice_is_the_identity at square,
+    -- rewrite FunctorComposition.left_identity at square,
+    -- exact square
+
+    -- refine ( cast _ square ),
+    -- -- refine ( transport _ square ),
+    -- rewrite - FunctorComposition.associativity,
+    -- rewrite switch_twice_is_the_identity,
+    -- rewrite FunctorComposition.left_identity,
   end 
 
 -- set_option pp.implicit true
@@ -52,7 +101,7 @@ set_option trace.check true
 --           (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C)
 --              (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))))
 -- T === (λ (_x : Type (max u v)), _x)
--- t : a
+-- t : T a == a
 -- t === (@vertical_composition_of_NaturalTransformations (C × C) C (@MonoidalStructure.tensor C m)
 --           (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
 --           (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C)
@@ -68,45 +117,211 @@ set_option trace.check true
 -- b === (@NaturalTransformation (C × C) C (@MonoidalStructure.tensor C m) (@MonoidalStructure.tensor C m))
 -- e : a = b
 
--- @eq.rec α a T b e : T b
+-- @eq.rec α a T t b e : T b
 -- T b == b
 
 -- now we want to apply
 --   f === @NaturalTransformation.components (C × C) C (@MonoidalStructure.tensor C m) (@MonoidalStructure.tensor C m)
--- to T b.
+-- to @eq.rec α a T b e.
 -- actually, we're going to need to think of this as applying 
 --   f' === @NaturalTransformation.components (C × C) C (@MonoidalStructure.tensor C m)
 --      : Π (F : Functor (C × C) C), NaturalTransformation m.tensor F → Π (x : (C × C).Obj), C.Hom (m.tensor x) (F x)
--- and here we may as well introduce S : Functor (C × C) C → Type as S = 
+-- and here we may as well introduce S : Functor (C × C) C → Type as S F = Π (x : (C × C).Obj), C.Hom (m.tensor x) (F x),
+-- and then write f' : Π (F : Functor (C × C) C), NaturalTransformation m.tensor F → S F 
+--
+-- Thus we want to start with f' (@MonoidalStructure.tensor C m) (@eq.rec α a T t b e) : S (@MonoidalStructure.tensor C m)
+-- and by application of the lemma we're designing obtain ...?
+--   f' (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C)
+--              (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m)))
+--      a 
+-- Now this thing is of type S (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C)
+--              (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m)))
+-- which is no good, so we're going to have to transport it through the type family S. But where do we obtain our 
+-- path/equation to transport along? Somehow we take e : a = b, and think of that actually as e : R a' = R b' ... nope.
 
 -------------
 
--- f === @NaturalTransformation.components (C × C) C (@MonoidalStructure.tensor C m)
--- What sort of thing is f?
--- f : Π F : (Functor (C × C) C), S F → T F
--- for two different type families S and T.
+-- 20170508
+--
+-- (@NaturalTransformation.components (C × C) C
+--        (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
+--        (@MonoidalStructure.tensor C m)
+--        (@eq.rec (Functor (C × C) C)
+--           (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+--           (@NaturalTransformation (C × C) C
+--              (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m)))
+--           (t : NaturalTransformation (switch tensor) (id tensor))
+--           (@MonoidalStructure.tensor C m)
+--           (FunctorComposition.left_identity (C × C) C (@MonoidalStructure.tensor C m)))
+--        X)
 
--- Now b : S F1, while a : S F2, and e : a = b
+-- S = (@NaturalTransformation (C × C) C
+-- f = @NaturalTransformation.components (C × C) C
+-- f : Π a a' : A, Π t : S a a', Z a a'
 
--- (@eq.rec β c S s d f) : S d
+-- Z a a' = Π X : (C × C).Obj, C.Hom (a X) (a' X)
 
--- but it also has to be of type T a.
+-- a  = (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
+-- a' = (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
 
--- lemma foo : { β : Type } { c : α } { S : β → Type } ( s : S c ) { d : β } ( f : c = d )
---             { α : Type } 
--- @eq.rec α a T (@eq.rec β c S s d f) b e
+-- A = (Functor (C × C) C)
+-- a a' : A 
+
+-- b = (@MonoidalStructure.tensor C m)
+-- b : A
+
+-- p : a' = b
+
+-- t : S a a'
+
+-- (@eq.rec A a' (S a) t b p) : S a b
+-- f a b (@eq.rec A a' (S a) t b p) : Z a b
+-- f a a' t : Z a a'
+-- (@eq.rec A a' (Z a) (f a a' t) b p) : Z a b
 
 
--- (@eq.rec (Functor (C × C) C) (@MonoidalStructure.tensor C m)
---                 (λ (a : Functor (C × C) C), a = @MonoidalStructure.tensor C m)
---                 (@eq.refl (TensorProduct C) (@MonoidalStructure.tensor C m))
+
+
+
+lemma {u1 u2 u3 u4} v2 { α : Type u1 } { β : Type u2 } { S : α → β → Type u3 } { Z : α → β → Type u4 } { a : α } { b : β } { t : S a b } { b' : β } { p : b = b' } ( f : Π ( c : α ) ( d : β ) , S c d → Z c d ): 
+  f a b' (@eq.rec β b (S a) t b' p) = (@eq.rec β b (Z a) (f a b t) b' p) :=
+begin
+induction p, reflexivity
+end
+
+lemma {u1 u2 u3} v1 { β : Type u1 } { S : β → Type u2 } { Z : β → Type u3 } { b : β } { t : S b } { b' : β } { p : b = b' } ( f : Π d : β, S d → Z d ) : 
+  f b' (@eq.rec β b S t b' p) = (@eq.rec β b Z (f b t) b' p) :=
+begin
+induction p, reflexivity
+end
+
+
+      --  (@NaturalTransformation.components (C × C) C
+      --     (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
+      --     (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+      --     (@eq.rec (Functor (C × C) (C × C))
+      --        (@FunctorComposition (C × C) (C × C) (C × C) (SwitchProductCategory C C) (SwitchProductCategory C C))
+      --        (λ (a : Functor (C × C) (C × C)),
+      --           @NaturalTransformation (C × C) C
+      --             (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
+      --             (@FunctorComposition (C × C) (C × C) C a (@MonoidalStructure.tensor C m)))
+      --        (t : NaturalTransformation (switch tensor) (switch switch tensor))
+      --        (IdentityFunctor (C × C))
+      --        (switch_twice_is_the_identity C C)))
+
+-- f  = @NaturalTransformation.components (C × C) C
+-- α  = Functor (C × C) C 
+-- a  : α
+-- a  = (@FunctorComposition (C × C) (C × C) C (SwitchProductCategory C C) (@MonoidalStructure.tensor C m))
+-- β  = Functor (C × C) C
+-- f  : Π (a : α) (b : β), S a b → Z a b
+-- γ  = (Functor (C × C) (C × C)
+-- g  = switch switch
+-- g' = id
+-- p  : g = g'
+-- R  : γ → β
+--    = λ g, (g tensor)
+-- S  = λ a b, NaturalTransformation a b
+--
+-- f a (R g') (@eq.rec γ g (S a) t g' p)
+
+
+-- c' = (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+-- c'' = (@FunctorComposition (C × C) (C × C) C (@FunctorComposition (C × C) (C × C) (C × C) (SwitchProductCategory C C) (SwitchProductCategory C C)) (@MonoidalStructure.tensor C m))
+-- eq.rec ... : NaturalTransformation c c'
+-- γ = (Functor (C × C) (C × C)
+-- g = switch switch
+-- S = Π (a : α) (g : γ), NaturalTransformation a (R g)
+-- t : NaturalTransformation a (R g)
+-- t : S a g
+-- g' = Identity (C × C)
+-- p : g = g'
+
+-- c' = R g'
+-- c'' = R g
+-- R = Π g : γ, (g tensor)
+-- R : γ → β
+
+-- f a c' t' : Z a c'
+
+-- f a (R g') (@eq.rec γ g S t g' p)
+-- f a (@eq.rec γ g R c'' g' p) (@eq.rec γ g S t g' p)
+-- @eq.rec γ g (Z a (R g)) (f a (R g) t) g' p
+-- @eq.rec γ g (Z a ???) (f a (@eq.rec γ g' R c' g (eq.symm p)) t) g' p
+
+
+
+lemma {u1 u2 u3 u4 u5} w
+  { α : Type u1 } { β : Type u2 } { γ : Type u3 }
+  { S : α → β → Type u4 }
+  { Z : α → β → Type u5 }
+  { a : α } { g : γ } ( R : γ → β ) { t : S a (R g) } { g' : γ } { p : g = g' } ( f : Π (a : α) (b : β), S a b → Z a b ):
+  f a (R g') (@eq.rec γ g (λ g, S a (R g)) t g' p) 
+    = 
+  @eq.rec γ g (λ g, Z a (R g)) (f a (R g) t) g' p
+    := begin induction p, reflexivity end
+
+--
+-- (@eq.rec (Functor (C × C) C)
 --                 (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
---                 (@eq.symm (Functor (C × C) C)
---                    (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
---                    (@MonoidalStructure.tensor C m)
---                    (FunctorComposition.left_identity (C × C) C (@MonoidalStructure.tensor C m))))
+--                 (λ (G : Functor (C × C) C),
+--                    Π (X : (C × C).Obj),
+--                      C.Hom
+--                        (@Functor.onObjects (C × C) C
+--                           (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C))
+--                              (@MonoidalStructure.tensor C m))
+--                           X)
+--                        (@Functor.onObjects (C × C) C G X))
+--                 (λ (X : (C × C).Obj),
+--                    C.identity
+--                      (@Functor.onObjects (C × C) C
+--                         (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C))
+--                            (@MonoidalStructure.tensor C m))
+--                         X))
+--                 (@MonoidalStructure.tensor C m)
+--                 (@squared_Braiding._proof_3 C m)
+--                 X)
+lemma {u1 u2 u3} b { α : Type u1 } { a : α } { β : Type u2 } { Z : α → β → Type u3 } { t : Π (b : β), Z a b } { a' : α } { p : a = a' } 
+  : @eq.rec α a _ t a' p = λ b : β, @eq.rec α a (λ _a, Z _a b) (t b) a' p :=
+begin
+  induction p,
+  reflexivity
+end
 
+--  (@eq.rec (Functor (C × C) C)
+--                 (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+--                 (λ (_a : Functor (C × C) C),
+--                    C.Hom
+--                      (@Functor.onObjects (C × C) C
+--                         (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C))
+--                            (@MonoidalStructure.tensor C m))
+--                         (X_1, X_2))
+--                      (@Functor.onObjects (C × C) C _a (X_1, X_2)))
+--                 (C.identity
+--                    (@Functor.onObjects (C × C) C
+--                       (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C))
+--                          (@MonoidalStructure.tensor C m))
+--                       (X_1, X_2)))
+--                 (@MonoidalStructure.tensor C m)
+--                 (@squared_Braiding._proof_3 C m))
+-- α = (Functor (C × C) C)
+-- a = (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+-- f = C.identity
+--   : Π X : C.Obj, C.Hom X X
+-- r = @Functor.onObjects (C × C) C
+-- x : (C × C).Obj
+-- r a x : C.Obj
+-- t = f (r a x)
+--   : C.Hom (r a x) (r a x)
 
+lemma {u1 u2 u3 u4} pull_function_out
+  { α : Type u1 } { a : α } { β : Type u2 } { Z : β → Type u3 } { γ : Type u4 } { r : Π a : α, γ → β } { x : γ } { a' : α } { p : a = a' } ( f : Π (b : β), Z b ) :
+  @eq.rec α a _ (f (r a x)) a' p = f (r a' x) := 
+begin
+induction p, reflexivity
+end
+
+-- This seems sensible, but applies too often.
 lemma {u} bar { α : Type u } { a : α } { b : α } ( p : a = b ) : @eq.rec α a (λ b: α, b = a) (eq.refl a) b p = eq.symm p :=
 begin
   cases p,
@@ -128,20 +343,75 @@ begin
   reflexivity
 end
 
+lemma z {A : Type} { X : A } : (eq.mpr (propext (eq_self_iff_true X)) trivial) = rfl :=
+begin
+  -- unfold trivial,
+  reflexivity
+end
 
+set_option pp.implicit true
 
 lemma {u v} symmetry_in_terms_of_natural_transformations { C : Category.{u v} } { m : MonoidalStructure C } ( β : Symmetry m ) : squared_Braiding (β.braiding) = IdentityNaturalTransformation m.tensor := 
   begin
     apply NaturalTransformations_componentwise_equal,
     intros,
     dsimp,
-    unfold_unfoldable_hypotheses,
-    induction X with X1 X2,
+
+    unfold vertical_composition_of_NaturalTransformations,
+    dsimp,
+
     unfold squared_Braiding._proof_1,
-    unfold transport,
-    -- rewrite bar (eq.symm (FunctorComposition.left_identity (C × C) C (m.tensor))),
-    -- rewrite foo,
-    rewrite function_of_eq.rec,
+    unfold squared_Braiding._proof_2,
+
+
+    unfold whisker_on_right,
+    unfold IdentityNaturalTransformation,
+    unfold horizontal_composition_of_NaturalTransformations,
+    dsimp,
+    rewrite v2 (@NaturalTransformation.components (C × C) C),
+    rewrite v2 (@NaturalTransformation.components (C × C) C),
+    simp,
+    -- unfold squared_Braiding._proof_3,
+    -- unfold FunctorComposition.left_identity,
+    dsimp,
+    rewrite b,
+    rewrite b,
+    simp,
+    induction X with X_1 X_2,
+    rewrite
+      @pull_function_out
+        (Functor (C × C) C)
+        (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C)) (@MonoidalStructure.tensor C m))
+        C.Obj
+        (λ (_a : Functor (C × C) C),
+                   C.Hom
+                     (@Functor.onObjects (C × C) C
+                        (@FunctorComposition (C × C) (C × C) C (IdentityFunctor (C × C))
+                           (@MonoidalStructure.tensor C m))
+                        (X_1, X_2))
+                     (@Functor.onObjects (C × C) C _a (X_1, X_2)))
+        (C × C).Obj
+        (@Functor.onObjects (C × C) C)
+        (X_1, X_2)
+        (@MonoidalStructure.tensor C m)
+        (@squared_Braiding._proof_3 C m)
+        C.identity
+        ,
+    -- unfold SwitchProductCategory,
+    -- unfold whisker_on_left,
+    -- unfold horizontal_composition_of_NaturalTransformations,
+    -- unfold IdentityNaturalTransformation,
+    -- unfold FunctorComposition,
+    -- unfold ProductCategory,
+    -- unfold IdentityFunctor,
+    -- dsimp,
+
+
+    -- rewrite v2 (@NaturalTransformation.components (C × C) (C × C)),
+    -- rewrite w
+    --   (λ (a : Functor (C × C) (C × C)), (@FunctorComposition (C × C) (C × C) C a (@MonoidalStructure.tensor C m)))
+    --   (@NaturalTransformation.components (C × C) C),
+
     -- At this point we have an unpleasant goal involving many eq.rec's, that I can't seem to do anything with.
     -- C : Category,
     -- m : MonoidalStructure C,
