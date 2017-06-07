1b8870ae272271995639abd9fef2c8ca319a4b92
Scott Morrison <scott.morrison@anu.edu.au>
2017-06-07 11:42:38 +1000

    commenting out stubs for Fubini

--- src/category_theory/universal/fubini.lean
+++ src/category_theory/universal/fubini.lean
@@ -16,51 +16,52 @@ open tqft.categories.natural_transformation
 
 namespace tqft.categories.universal
 
-structure iterated_limit_for_bifunctor 
-  { J K : Category }
-  { C : Category }
-  ( F : Functor (J × K) C ) :=
-  ( limitFunctor : Limit (Curry_Functors J K C F) )
-  ( limitObject  : Limit (limitFunctor.object.limit) )
+-- structure iterated_limit_for_bifunctor 
+--   { J K : Category }
+--   { C : Category }
+--   ( F : Functor (J × K) C ) :=
+--   ( limitFunctor : Limit (Curry_Functors J K C F) )
+--   ( limitObject  : Limit (limitFunctor.object.limit) )
 
-definition Fubini_for_Limits
-  { J K : Category }
-  { C : Category }
-  { F : Functor (J × K) C }
-  ( lim : Limit F ) : iterated_limit_for_bifunctor F := {
-      limitFunctor := {
-          object     := {
-              limit         := {
-                  onObjects     := sorry,
-                  onMorphisms   := sorry,
-                  identities    := sorry,
-                  functoriality := sorry
-              },
-              maps          := sorry,
-              commutativity := sorry
-          },
-          morphisms  := sorry,
-          uniqueness := sorry
-      },
-      limitObject  := sorry
-  }
+-- -- The dependent sorries here cause a problem described at https://github.com/leanprover/lean/issues/1650
+-- definition Fubini_for_Limits
+--   { J K : Category }
+--   { C : Category }
+--   { F : Functor (J × K) C }
+--   ( lim : Limit F ) : iterated_limit_for_bifunctor F := {
+--       limitFunctor := {
+--           object     := {
+--               limit         := {
+--                   onObjects     := sorry,
+--                   onMorphisms   := sorry,
+--                   identities    := sorry,
+--                   functoriality := sorry
+--               },
+--               maps          := sorry,
+--               commutativity := sorry
+--           },
+--           morphisms  := sorry,
+--           uniqueness := sorry
+--       },
+--       limitObject  := sorry
+--   }
 
-definition Fubini_for_Limits_inverse
-  { J K : Category }
-  { C : Category }
-  { F : Functor (J × K) C }
-  ( lim : iterated_limit_for_bifunctor F ) : Limit F := sorry
+-- definition Fubini_for_Limits_inverse
+--   { J K : Category }
+--   { C : Category }
+--   { F : Functor (J × K) C }
+--   ( lim : iterated_limit_for_bifunctor F ) : Limit F := sorry
 
-lemma Fubini_for_Limits.objects_isomorphic
-  { J K : Category }
-  { C : Category }
-  { F : Functor (J × K) C }
-  ( lim : Limit F ) : Isomorphism C lim.object.limit (Fubini_for_Limits lim).limitObject.object.limit := sorry
+-- lemma Fubini_for_Limits.objects_isomorphic
+--   { J K : Category }
+--   { C : Category }
+--   { F : Functor (J × K) C }
+--   ( lim : Limit F ) : Isomorphism C lim.object.limit (Fubini_for_Limits lim).limitObject.object.limit := sorry
 
-lemma Fubini_for_Limits_inverse.objects_isomorphic
-  { J K : Category }
-  { C : Category }
-  { F : Functor (J × K) C }
-  ( lim : iterated_limit_for_bifunctor F ) : Isomorphism C lim.limitObject.object.limit (Fubini_for_Limits_inverse lim).object.limit := sorry
+-- lemma Fubini_for_Limits_inverse.objects_isomorphic
+--   { J K : Category }
+--   { C : Category }
+--   { F : Functor (J × K) C }
+--   ( lim : iterated_limit_for_bifunctor F ) : Isomorphism C lim.limitObject.object.limit (Fubini_for_Limits_inverse lim).object.limit := sorry
 
 end tqft.categories.universal
