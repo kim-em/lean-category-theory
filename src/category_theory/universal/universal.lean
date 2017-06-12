7b23ea67a0df679d0095ed0dd2e0310c8244a098
Scott Morrison <scott.morrison@anu.edu.au>
2017-06-12 11:21:47 -0700

    finish the construction of limits from products and equalisers

--- src/category_theory/universal/universal.lean
+++ src/category_theory/universal/universal.lean
@@ -318,60 +318,62 @@ open Two
 -- }
 
 
--- PROJECT finish proving that this constructs limits from products and equalizers
--- instance Limits_from_Products_and_Equalizers ( C : Category ) [ has_Products C ] [ has_Equalizers C ] : Complete C := {
---   limit := λ J F,
---     let product_over_objects   := product (F.onObjects) in
---     let product_over_morphisms := product (λ f : ( Σ s : J.Obj, Σ t : J.Obj, J.Hom s t ), F.onObjects f.2.1) in
---     let source    := product_over_morphisms.map (λ f, C.compose (product_over_objects.projection f.1) (F.onMorphisms f.2.2) )  in
---     let target    := product_over_morphisms.map (λ f, product_over_objects.projection f.2.1 ) in
---     let equalizer := equalizer source target in {
---       object     := {
---         limit         := equalizer.equalizer,
---         maps          := λ j : J.Obj, C.compose equalizer.inclusion (product_over_objects.projection j),
---         commutativity := λ j k f, begin
---                                    -- PROJECT learn how to use have, show, and calc!
---                                    note r : C.compose source (product_over_morphisms.projection ⟨j, ⟨k, f⟩⟩) = C.compose (product_over_objects.projection j) (F.onMorphisms f) := ♯,
---                                    note s : C.compose target (product_over_morphisms.projection ⟨j, ⟨k, f⟩⟩) = product_over_objects.projection k := ♯,
---                                    note p := congr_arg (λ i, C.compose i (product_over_morphisms.projection ⟨ j, ⟨ k, f ⟩ ⟩)) equalizer.witness,
---                                    simp at p,
---                                    repeat { rewrite C.associativity at p },
---                                    rewrite [ r, s ] at p,
---                                    rewrite - C.associativity at p,
---                                    exact p,
---                                   end
---       },
---       morphisms  := λ cone : Cone F, {
---         morphism      := /- we need a morphism from the tip of f to the equalizer -/
---                          equalizer.map
---                            (product_over_objects.map cone.maps)
---                            /- we need to provide the evidence that that map composes correctly with source and target -/
---                            begin
---                              revert source, revert target,
---                              simp,
---                              note p := cone.commutativity,
---                              admit -- FIXME almost there!
---                            end,
---         commutativity := /- we need to show that that map commutes with everything -/
---           begin
---             intros,
---             rewrite - C.associativity,
---             simp,
---           end
---       },
---       uniqueness := λ cone f g, begin
---                                   pointwise,
---                                   pointwise,
---                                   pointwise,
---                                   intros,
---                                   note u := f.commutativity i,
---                                   note v := g.commutativity i,
---                                   unfold_projections_hypotheses,
---                                   repeat { rewrite C.associativity },
---                                   rewrite [ u, v ],
---                                 end
---     }
--- }
+instance Limits_from_Products_and_Equalizers ( C : Category ) [ has_Products C ] [ has_Equalizers C ] : Complete C := {
+  limit := λ J F,
+    let product_over_objects   := product (F.onObjects) in
+    let product_over_morphisms := product (λ f : ( Σ s : J.Obj, Σ t : J.Obj, J.Hom s t ), F.onObjects f.2.1) in
+    let source    := product_over_morphisms.map (λ f, C.compose (product_over_objects.projection f.1) (F.onMorphisms f.2.2) )  in
+    let target    := product_over_morphisms.map (λ f, product_over_objects.projection f.2.1 ) in
+    let source_projection_lemma : ∀ t : ( Σ s : J.Obj, Σ t : J.Obj, J.Hom s t ), C.compose source (product_over_morphisms.projection t) = C.compose (product_over_objects.projection t.1) (F.onMorphisms t.2.2) := ♯ in
+    let target_projection_lemma : ∀ t : ( Σ s : J.Obj, Σ t : J.Obj, J.Hom s t ), C.compose target (product_over_morphisms.projection t) = product_over_objects.projection t.2.1 := ♯ in
+    let equalizer := equalizer source target in {
+      object     := {
+        limit         := equalizer.equalizer,
+        maps          := λ j : J.Obj, C.compose equalizer.inclusion (product_over_objects.projection j),
+        commutativity := λ j k f, begin
+                                   -- PROJECT learn how to use have, show, and calc!
+                                   note p := congr_arg (λ i, C.compose i (product_over_morphisms.projection ⟨ j, ⟨ k, f ⟩ ⟩)) equalizer.witness,
+                                   simp at p,
+                                   repeat { rewrite C.associativity at p },
+                                   rewrite [ source_projection_lemma, target_projection_lemma ] at p,
+                                   rewrite - C.associativity at p,
+                                   exact p,
+                                  end
+      },
+      morphisms  := λ cone : Cone F, {
+        morphism      := /- we need a morphism from the tip of f to the equalizer -/
+                         equalizer.map
+                           (product_over_objects.map cone.maps)
+                           /- we need to provide the evidence that that map composes correctly with source and target -/
+                           begin
+                             pointwise,
+                             intros,
+                             repeat { rewrite C.associativity },
+                             rewrite [ source_projection_lemma, target_projection_lemma ],
+                             rewrite - C.associativity,
+                             repeat { rewrite product_over_objects.factorisation },
+                             rewrite cone.commutativity,
+                           end,
+        commutativity := /- we need to show that that map commutes with everything -/
+          begin
+            intros,
+            rewrite - C.associativity,
+            simp,
+          end
+      },
+      uniqueness := λ cone f g, begin
+                                  pointwise,
+                                  pointwise,
+                                  pointwise,
+                                  intros,
+                                  note u := f.commutativity i,
+                                  note v := g.commutativity i,
+                                  unfold_projections_hypotheses,
+                                  repeat { rewrite C.associativity },
+                                  rewrite [ u, v ],
+                                end
+    }
+}
 
 
 end tqft.categories.universal
