f89e02b73c32a7b3d30e74a1591b2f0faea6aa15
Scott Morrison <scott.morrison@anu.edu.au>
2017-06-13 11:31:38 -0700

    first attempt at the definition of an abelian category

--- src/category_theory/abelian/abelian.lean
+++ src/category_theory/abelian/abelian.lean
@@ -7,17 +7,21 @@ import ..universal.universal
 open tqft.categories
 open tqft.categories.initial
 open tqft.categories.universal
+open tqft.categories.isomorphism
 
 namespace tqft.categories.abelian
 
--- structure KernelImageCokernelSequence { C : Category } [ ZeroObject C ] { X Y : C.Obj } ( f : C.Hom X Y )
---   (kernel   : Kernel f  )
---   (cokernel : Cokernel f)
---   (cokernel_of_kernel : Cokernel (kernel.inclusion))
---   (kernel_of_cokernel : Kernel (cokernel.projection))
---   (image_well_defined : cokernel_of_kernel.coequalizer = kernel_of_cokernel.equalizer)
---   (composition_is_morphism : )
+-- This is the definition of abelian from Etingof's "Tensor categories"
 
--- structure Abelian ( C : Category ) :=
+structure KernelImageCokernelSequence { C : Category } [ ZeroObject C ] { X Y : C.Obj } ( f : C.Hom X Y ) :=
+  ( kernel                  : Kernel f   )
+  ( cokernel                : Cokernel f )
+  ( cokernel_of_kernel      : Cokernel (kernel.inclusion)  )
+  ( kernel_of_cokernel      : Kernel (cokernel.projection) )
+  ( image_well_defined      : Isomorphism C cokernel_of_kernel.coequalizer kernel_of_cokernel.equalizer )
+  ( composition_is_morphism : C.compose (C.compose cokernel_of_kernel.projection image_well_defined.morphism) kernel_of_cokernel.inclusion = f )
+
+structure Abelian ( C : Category ) [ ZeroObject C ] := 
+  ( decomposition : ∀ { X Y : C.Obj } ( f : C.Hom X Y ), KernelImageCokernelSequence f )
 
 end tqft.categories.abelian
