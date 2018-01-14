0117c3885197f4fcce27edff6911f98daa729460
Scott Morrison <scott@tqft.net>
2018-01-14 20:36:50 +1100

    moving examples from the monoidal-categories library to here

---
+++ src/categories/examples/monoids.lean
@@ -0,0 +1,77 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Stephen Morgan, Scott Morrison
+
+import categories.functor
+import .semigroups
+
+namespace categories.examples.monoids
+
+open categories
+
+structure monoid_morphism { α β : Type } ( s : monoid α ) ( t : monoid β ) :=
+  (map: α → β)
+  (multiplicative : ∀ x y : α, map (monoid.mul x y) = monoid.mul (map x) (map y))
+  (unital : map(s.one) = t.one)
+
+attribute [simp] monoid_morphism.multiplicative
+attribute [simp] monoid_morphism.unital
+
+-- This defines a coercion so we can write `f x` for `map f x`.
+instance monoid_morphism_to_map { α β : Type } { s : monoid α } { t : monoid β } : has_coe_to_fun (monoid_morphism s t) :=
+{ F   := λ f, Π x : α, β,
+  coe := monoid_morphism.map }
+
+definition monoid_identity { α : Type } ( s : monoid α ) : monoid_morphism s s := ⟨ id, ♮, ♮ ⟩
+
+definition monoid_morphism_composition
+  { α β γ : Type } { s : monoid α } { t : monoid β } { u : monoid γ}
+  ( f: monoid_morphism s t ) ( g: monoid_morphism t u ) : monoid_morphism s u :=
+{
+  map := λ x, g (f x),
+  multiplicative := ♯,
+  unital := ♯
+}
+
+@[applicable] lemma monoid_morphism_pointwise_equality
+  { α β : Type } { s : monoid α } { t : monoid β }
+  ( f g : monoid_morphism s t )
+  ( w : ∀ x : α, f x = g x) : f = g :=
+begin
+    induction f with fc,
+    induction g with gc,
+    have hc : fc = gc := funext w,
+    subst hc
+end
+
+definition CategoryOfMonoids : Category := 
+{
+    Obj := Σ α : Type, monoid α,
+    Hom := λ s t, monoid_morphism s.2 t.2,
+
+    identity := λ s, monoid_identity s.2,
+    compose  := λ _ _ _ f g, monoid_morphism_composition f g,
+
+    left_identity  := ♯,
+    right_identity := ♯,
+    associativity  := ♮
+}
+
+open categories.functor
+open categories.examples.semigroups
+
+definition cast_monoid_to_semigroup { α : Type } ( s : monoid α ) : semigroup α := @monoid.to_semigroup α s
+
+definition ForgetfulFunctor_Monoids_to_Semigroups : Functor CategoryOfMonoids CategoryOfSemigroups :=
+{
+  onObjects     := λ s, sigma.mk s.1 (@monoid.to_semigroup s.1 s.2),
+  onMorphisms   := λ s t, λ f : monoid_morphism s.2 t.2, 
+                  {
+                    map            := f.map,
+                    multiplicative := f.multiplicative
+                  },
+  identities    := ♮,
+  functoriality := ♮
+}
+
+end categories.examples.monoids
---
+++ src/categories/examples/semigroups.lean
@@ -0,0 +1,61 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Stephen Morgan, Scott Morrison
+import categories.category
+
+namespace categories.examples.semigroups
+
+open categories
+
+structure {u} semigroup_morphism { α β : Type u } ( s : semigroup α ) ( t: semigroup β ) :=
+  (map: α → β)
+  (multiplicative : ∀ x y : α, map (semigroup.mul x y) = semigroup.mul (map x) (map y))
+
+attribute [simp,ematch] semigroup_morphism.multiplicative
+
+definition {u} monoid_semigroup_to_map { α β : Type u } { s : semigroup α } { t: semigroup β } : has_coe_to_fun (semigroup_morphism s t) :=
+{ F   := λ f, Π x : α, β,
+  coe := semigroup_morphism.map }
+
+attribute [instance] monoid_semigroup_to_map
+
+definition {u} semigroup_identity { α : Type u } ( s: semigroup α ) : semigroup_morphism s s := ⟨ id, ♮ ⟩
+
+definition {u} semigroup_morphism_composition
+  { α β γ : Type u } { s: semigroup α } { t: semigroup β } { u: semigroup γ}
+  ( f: semigroup_morphism s t ) ( g: semigroup_morphism t u ) : semigroup_morphism s u :=
+{
+  map := λ x, g (f x),
+  multiplicative := ♯
+}
+
+@[applicable] lemma {u} semigroup_morphism_pointwise_equality
+  { α β : Type u } { s : semigroup α } { t: semigroup β }
+  ( f g : semigroup_morphism s t )
+  ( w : ∀ x : α, f x = g x) : f = g :=
+begin
+    induction f with fc,
+    induction g with gc,
+    have hc : fc = gc := funext w,
+    subst hc
+end
+
+definition {u} CategoryOfSemigroups : Category := 
+{
+    Obj := Σ α : Type u, semigroup α,
+    Hom := λ s t, semigroup_morphism s.2 t.2,
+
+    identity := λ s, semigroup_identity s.2,
+    compose  := λ _ _ _ f g, semigroup_morphism_composition f g,
+
+    left_identity  := ♯,
+    right_identity := ♯,
+    associativity  := ♮
+}
+
+definition trivial_semigroup: semigroup punit := {
+  mul := λ _ _, punit.star,
+  mul_assoc := ♮
+}
+
+end categories.examples.semigroups
