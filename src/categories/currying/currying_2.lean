2e4c71a4db105da358866cec1c5cfafad1f22e46
Scott Morrison <scott@tqft.net>
2018-01-15 16:16:50 +1100

    uncommenting-out Currying

--- src/categories/currying/currying_2.lean
+++ src/categories/currying/currying_2.lean
@@ -4,31 +4,33 @@
 
 -- TODO this was taking forever to compile
 
--- import .currying_1
+import .currying_1
 
--- open categories
--- open categories.isomorphism
--- open categories.functor
--- open categories.equivalence
+open categories
+open categories.isomorphism
+open categories.functor
+open categories.equivalence
 
--- namespace categories.natural_transformation
+namespace categories.natural_transformation
 
--- universes u1 v1 u2 v2 u3 v3
+universes u1 v1 u2 v2 u3 v3
 
--- variable C : Category.{u1 v1}
--- variable D : Category.{u2 v2}
--- variable E : Category.{u3 v3}
+variable C : Category.{u1 v1}
+variable D : Category.{u2 v2}
+variable E : Category.{u3 v3}
 
--- definition Curry_Uncurry_to_identity
---     : NaturalTransformation (FunctorComposition (Uncurry_Functors C D E) (Curry_Functors C D E)) (IdentityFunctor _) := ♯ 
+local attribute [applicable] Category.identity -- this is usually a bad idea, but just what we needed here
 
--- definition identity_to_Curry_Uncurry
---     : NaturalTransformation (IdentityFunctor _) (FunctorComposition (Uncurry_Functors C D E) (Curry_Functors C D E)) := ♯ 
+definition Curry_Uncurry_to_identity
+    : NaturalTransformation (FunctorComposition (Uncurry_Functors C D E) (Curry_Functors C D E)) (IdentityFunctor _) := ♯ 
 
--- definition Uncurry_Curry_to_identity
---     : NaturalTransformation (FunctorComposition (Curry_Functors C D E) (Uncurry_Functors C D E)) (IdentityFunctor _) := ♯ 
+definition identity_to_Curry_Uncurry
+    : NaturalTransformation (IdentityFunctor _) (FunctorComposition (Uncurry_Functors C D E) (Curry_Functors C D E)) := ♯ 
+
+definition Uncurry_Curry_to_identity
+    : NaturalTransformation (FunctorComposition (Curry_Functors C D E) (Uncurry_Functors C D E)) (IdentityFunctor _) := ♯ 
      
--- definition identity_to_Uncurry_Curry
---     : NaturalTransformation (IdentityFunctor _) (FunctorComposition (Curry_Functors C D E) (Uncurry_Functors C D E)) := ♯ 
+definition identity_to_Uncurry_Curry
+    : NaturalTransformation (IdentityFunctor _) (FunctorComposition (Curry_Functors C D E) (Uncurry_Functors C D E)) := ♯ 
 
--- end categories.natural_transformation
+end categories.natural_transformation
