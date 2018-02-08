1e422969e3e4f0ae0ab724ba0506e7ddfbfbe2d8
Scott Morrison <scott@tqft.net>
2018-02-09 10:28:20 +1100

    removing a sorry; still gross

--- src/categories/examples/naturals.lean
+++ src/categories/examples/naturals.lean
@@ -2,7 +2,7 @@
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Stephen Morgan, Scott Morrison
 import categories.natural_transformation
-
+import init.data.nat.lemmas
 namespace categories.examples.naturals
 
 open categories
@@ -22,13 +22,20 @@ unfold nat.add,
 rw a_ih,
 end
 
-@[simp] private lemma zero_add (a b : ℕ) : ((nat.add b a) = a) ↔ (b = 0) := 
+@[simp] private lemma zero_add_iff (a b : ℕ) : ((nat.add b a) = a) ↔ (b = 0) := 
 begin
 tidy,
-sorry,
-rw a_1, simp
+{
+have q : nat.add b a = a + b, by tidy,
+rw q at a_1,
+have p : a = a + 0, by simp,
+rw p at a_1,
+have r : a + 0 + b = a + b, by simp,
+rw r at a_1,
+exact nat.add_left_cancel a_1
+},
+{ rw a_1, simp }
 end
-
 
 -- PROJECT This reducible is gross, but without it we can't see what NCategory.Hom is...
 @[reducible] definition ℕCategory : Category :=
