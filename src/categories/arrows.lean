9feb7ba23b528b024e1b63b8faf53f86730e77dd
Scott Morrison <scott@tqft.net>
2018-02-15 17:12:06 +1100

    minor

--- src/categories/arrows.lean
+++ src/categories/arrows.lean
@@ -3,10 +3,12 @@
 -- Authors: Scott Morrison
 
 import .natural_transformation
+import .functor_categories
 
 open categories
 open categories.functor
 open categories.natural_transformation
+open categories.functor_categories
 
 namespace categories.arrows
 
@@ -18,8 +20,7 @@ local attribute [applicable] category.identity -- This says that whenever there 
 
 definition arrows (C : Type u₁) [category C] := Σ (p : C × C), Hom p.1 p.2
 
-definition Arrows (C : Type u₁) [category C] : category (arrows C):=
-{
+definition Arrows (C : Type u₁) [category C] : category (arrows C):= {
   Hom := λ X Y, {f : (Hom X.1.1 Y.1.1 × Hom X.1.2 Y.1.2) // f.1 >> Y.2 = X.2 >> f.2},
   identity       := by tidy,
   compose        := λ X Y Z f g, ⟨ (f.val.1 >> g.val.1, f.val.2 >> g.val.2), ♯ ⟩ 
