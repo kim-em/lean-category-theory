76c15c81c60765f231ccf5bb8875aef846f26887
Scott Morrison <scott@tqft.net>
2018-02-27 15:30:01 +1100

    cleanup

--- src/categories/walking.lean
+++ src/categories/walking.lean
@@ -4,6 +4,7 @@
 
 import .functor
 import .util.finite
+import data.fintype
 
 open categories
 open categories.functor
@@ -54,27 +55,15 @@ by tidy
 
 
 open tactic
--- private meta def induction_WalkingPair : tactic unit :=
--- do l ← local_context,
---    at_least_one (l.reverse.map (λ h, do t ← infer_type h, match t with | `(WalkingPair) := induction h >> skip | _ := failed end))
+private meta def induction_WalkingPair : tactic unit :=
+do l ← local_context,
+   at_least_one (l.reverse.map (λ h, do t ← infer_type h, match t with | `(WalkingPair) := induction h >> skip | _ := failed end))
 
--- attribute [tidy] induction_WalkingPair
+attribute [tidy] induction_WalkingPair
 
--- instance decidable_eq_WalkingPair : decidable_eq WalkingPair := ♯
+instance decidable_eq_WalkingPair : decidable_eq WalkingPair := ♯
+-- instance fintype_WalkingPair : fintype WalkingPair := sorry
 
--- instance WalkingPair_category : category WalkingPair := {
---   Hom := begin
---            intros X Y,
---            induction X,
---            {induction Y, exact punit, exact pempty},
---            {induction Y, exact pempty, exact punit}
---          end,
---   identity       := by tidy,
---   compose        := begin
---                       intros X Y Z f g, induction X ; induction Y ; induction Z ; dsimp at *, 
---                       exact punit.star, exact g, exact punit.star, exact f, induction f, exact punit.star, induction g, exact punit.star
---                     end
--- }
 instance WalkingPair_category : category WalkingPair := {
   Hom := λ X Y, match X, Y with
                 | _1, _1 := punit
@@ -85,7 +74,7 @@ instance WalkingPair_category : category WalkingPair := {
   compose        := begin
                       intros X Y Z f g, cases X; cases Y; cases Z; cases f; cases g; exact punit.star
                     end,
-  left_identity := begin dsimp, intros, cases X; cases Y; cases f; apply punits_equal end,
+  left_identity := begin dsimp, intros, cases X; cases Y; cases f; apply punits_equal end,  -- PROJECT these boring proofs should be handled by dec_trivial
   right_identity := begin dsimp, intros, cases X; cases Y; cases f; apply punits_equal end,
   associativity := begin dsimp, intros, cases W; cases X; cases Y; cases Z; cases f; cases g; cases h; apply punits_equal end,
 }
@@ -95,9 +84,6 @@ local attribute [applicable] category.identity
 
 variable {C : Type (u₁+1)}
 variable [category C]
-
--- @[simp] lemma Hom_1_2 : Hom _1 _2 = pempty := begin dunfold Hom, tidy, end
--- @[simp] lemma Hom_2_1 : Hom _2 _1 = pempty := begin dunfold Hom, tidy, end
 
 definition Pair_functor (α β : C) : Functor WalkingPair C := {
   onObjects     := λ X, match X with
@@ -111,19 +97,6 @@ definition Pair_functor (α β : C) : Functor WalkingPair C := {
   identities := begin dsimp, intros, cases X; refl, end,
   functoriality := begin dsimp, intros, cases X; cases Y; cases Z; cases f; cases g; unfold Pair_functor._match_2; simp, end,
 }
--- definition Pair_functor (α β : C) : Functor WalkingPair C := {
---   onObjects     := begin intros X, induction X, exact α, exact β end,
---   onMorphisms   := begin
---                      intros,
---                      induction X,
---                      {induction Y,
---                        {exact 𝟙 α},
---                        {induction a}},
---                      {induction Y,
---                        {induction a},
---                        {exact 𝟙 β}}
---                    end,
--- }
 end
 
 section
@@ -133,28 +106,6 @@ inductive WalkingParallelPair : Type u₁
 
 open WalkingParallelPair
 
--- open tactic
--- meta def induction_WalkingParallelPair : tactic unit :=
--- do l ← local_context,
---    at_least_one (l.reverse.map (λ h, do t ← infer_type h, match t with | `(WalkingParallelPair) := induction h >> skip | _ := failed end))
-
--- attribute [tidy] induction_WalkingParallelPair
-
--- instance decidable_eq_WalkingParallelPair : decidable_eq WalkingParallelPair := ♯
-
--- instance : category WalkingParallelPair := {
---   Hom := begin
---            intros X Y,
---            induction X,
---            {induction Y, exact punit, exact Two},
---            {induction Y, exact pempty, exact punit}
---          end,
---   identity       := by tidy,
---   compose        := begin
---                       intros X Y Z f g, induction X ; induction Y ; induction Z ; dsimp at *, 
---                       exact punit.star, exact g, exact punit.star, exact f, induction f, exact punit.star, induction g, exact punit.star
---                     end
--- }
 instance : category WalkingParallelPair := {
   Hom := λ X Y, match X, Y with
          | _1, _1 := punit
@@ -191,22 +142,6 @@ definition ParallelPair_functor {α β : C} (f g : Hom α β) : Functor WalkingP
   identities := begin dsimp, intros, cases X; unfold ParallelPair_functor._match_2; refl, end,
   functoriality := begin dsimp, intros, cases X; cases Y; cases Z; simp; cases f_1; cases g_1; unfold ParallelPair_functor._match_2, any_goals { rw category.identity_idempotent' }, any_goals { erw category.left_identity_lemma' }, any_goals { erw category.right_identity_lemma' }, end
 }
--- -- this style is obscene. FIXME learn to use match statements  (or rather, to automatically unfold them)
--- definition ParallelPair_functor {α β : C} (f g : Hom α β) : Functor WalkingParallelPair C := 
--- {
---   onObjects     := begin intros X, induction X, exact α, exact β end,
---   onMorphisms   := begin
---                      intros,
---                      induction X,
---                      {induction Y,
---                        {exact 𝟙 α},
---                        {induction a, exact f, exact g}},
---                      {induction Y,
---                        {induction a},
---                        {exact 𝟙 β}}
---                    end,
---   functoriality := begin tidy, any_goals { induction f_1 }, any_goals { induction g_1 },  end
--- }
 end
 
 end categories.walking
