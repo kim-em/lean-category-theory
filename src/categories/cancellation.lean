f44a52de1a8acf17bcc1965e8dc8e92e0b609423
Scott Morrison <scott@tqft.net>
2018-04-25 21:46:50 +1000

    eradicating its in favour of mathlib's convert

--- src/categories/cancellation.lean
+++ src/categories/cancellation.lean
@@ -2,7 +2,6 @@
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Scott Morrison
 
-import tidy.its
 import categories.category
 
 namespace categories
@@ -16,25 +15,25 @@ variable [category C]
 @[simp] def category.cancel_left (f g : X ⟶ Y) : (∀ {Z : C} (h : Y ⟶ Z), f ≫ h = g ≫ h) ↔ f = g :=
 begin
     split,
-    { intro w, its w (𝟙 Y), tidy },
+    { intro w, convert w (𝟙 Y), tidy },
     { obviously }
 end
 @[simp] def category.cancel_right (f g : Y ⟶ Z) : (∀ {X : C} (h : X ⟶ Y), h ≫ f = h ≫ g) ↔ f = g :=
 begin
     split,
-    { intro w, its w (𝟙 Y), tidy },
+    { intro w, convert w (𝟙 Y), tidy },
     { obviously }
 end
 @[simp] def category.identity.if_it_quacks_left (f : X ⟶ X) : (∀ {Y : C} (g : X ⟶ Y), f ≫ g = g) ↔ f = 𝟙 X :=
 begin
     split,
-    { intro w, its w (𝟙 X), tidy },
+    { intro w, convert w (𝟙 X), tidy },
     { obviously }
 end
 @[simp] def category.identity.if_it_quacks_right (f : X ⟶ X) : (∀ {Y : C} (g : Y ⟶ X), g ≫ f = g) ↔ f = 𝟙 X :=
 begin
     split,
-    { intro w, its w (𝟙 X), tidy },
+    { intro w, convert w (𝟙 X), tidy },
     { obviously }
 end
 
