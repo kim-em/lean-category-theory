3ed2c4b400b465093f2c62bd9cf6b7b9b9b173ac
Scott Morrison <scott@tqft.net>
2018-06-24 09:57:01 +1000

    shrinking currying again

---
+++ src/categories/currying.lean
@@ -0,0 +1,36 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Scott Morrison
+
+import categories.products.bifunctors
+import categories.equivalence
+
+open categories
+open categories.equivalence
+
+namespace categories.currying
+
+universes u₁ u₂ v₂ 
+
+variables (C : Type u₁) [small_category C] (D : Type u₁) [small_category D] (E : Type u₂) [ℰ : category.{u₂ v₂} E]
+include ℰ
+
+definition Uncurry_Functors : (C ↝ (D ↝ E)) ↝ ((C × D) ↝ E) := 
+{ onObjects     := λ F, { onObjects     := λ X, (F +> X.1) +> X.2,
+                          onMorphisms   := λ X Y f, ((F &> f.1).components X.2) ≫ ((F +> Y.1) &> f.2) },
+  onMorphisms   := λ F G T, { components := λ X, (T.components _).components _ } }
+
+definition Curry_Functors : ((C × D) ↝ E) ↝ (C ↝ (D ↝ E)) := 
+{ onObjects     := λ F, { onObjects     := λ X, { onObjects     := λ Y, F +> (X, Y),
+                                                  onMorphisms   := λ Y Y' g, F &> (𝟙 X, g) },
+                          onMorphisms   := λ X X' f, { components := λ Y, F.onMorphisms (f, 𝟙 Y) } },
+  onMorphisms   := λ F G T, { components := λ X, { components := λ Y, T.components (X, Y) } } }
+
+local attribute [applicable] category.identity -- this is usually a bad idea, but just what we needed here
+local attribute [tidy] dsimp_all'
+
+def Currying_for_functors : Equivalence (C ↝ (D ↝ E)) ((C × D) ↝ E) := 
+{ functor := Uncurry_Functors C D E,
+  inverse := Curry_Functors C D E }
+
+end categories.currying
