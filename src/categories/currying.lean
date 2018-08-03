2be9d5b0ddc396cef3e4c9a3f02f702b53ad00ea
Scott Morrison <scott.morrison@anu.edu.au>
2018-08-03 11:40:43 +1000

    restoring accidentally deleted lines

--- src/categories/currying.lean
+++ src/categories/currying.lean
@@ -12,6 +12,10 @@ open categories.functor
 
 namespace categories.currying
 
+universes u₁ u₂ v₂ 
+
+variables (C : Type u₁) [small_category C] (D : Type u₁) [small_category D] (E : Type u₂) [ℰ : category.{u₂ v₂} E]
+include ℰ
 
 -- set_option pp.notation false
 -- set_option pp.implicit true
