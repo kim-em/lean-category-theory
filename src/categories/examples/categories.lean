5ab75132236ecd7ced0e8cec870bea1acd58dad6
Scott Morrison <scott@tqft.net>
2018-08-08 22:04:33 +1000

    a better version of CAT

--- src/categories/examples/categories.lean
+++ src/categories/examples/categories.lean
@@ -1,13 +1,13 @@
 import category_theory.functor
 
-universes u
+universes u v
 
 namespace category_theory
 
-def Small_Category : Type (u+1) := Σ C : Type u, small_category C
-instance (𝒞 : Small_Category) : small_category (𝒞.1) := 𝒞.2
+def Category : Type (max (u+1) (v+1)) := Σ C : Type u, category.{u v} C
+instance (𝒞 : Category) : category (𝒞.1) := 𝒞.2
 
-instance : category Small_Category :=
+instance CAT : category.{(max (u+1) (v+1)) (max u v)} Category :=
 { Hom     := λ 𝒞 𝒟, 𝒞.1 ↝ 𝒟.1,
   id      := λ 𝒞, (functor.id 𝒞.1),
   comp    := λ _ _ _ F G, F ⋙ G,
