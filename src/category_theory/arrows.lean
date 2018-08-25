13fea58a367dc187efae727f928b4e47f50072c6
Scott Morrison <scott@tqft.net>
2018-08-25 15:22:20 +0200

    all new tidy

--- src/category_theory/arrows.lean
+++ src/category_theory/arrows.lean
@@ -6,7 +6,7 @@ import category_theory.natural_transformation
 import category_theory.functor_category
 import .tactics.obviously
 
-universes u₁ u₂
+universes u₁ v₁ u₂ v₂
 
 open category_theory
 
@@ -16,44 +16,45 @@ local attribute [back] category.id -- This says that whenever there is a goal of
 
 -- Is there any point defining these separately (rather than as the functor category from the walking arrow)?
 
-def arrow (C : Type (u₁+1)) [large_category C] := Σ (p : C × C), p.1 ⟶ p.2
+variables (C : Type u₁) [category.{u₁ v₁} C]
 
-structure arrow_hom {C : Type (u₁+1)} [large_category C] (X Y : arrow C) :=
+def arrow := Σ (p : C × C), p.1 ⟶ p.2
+
+variable {C}
+
+structure arrow_hom (X Y : arrow C) :=
 (morphism : (X.1.1 ⟶ Y.1.1) × (X.1.2 ⟶ Y.1.2))
 (commutativity : morphism.1 ≫ Y.2 = X.2 ≫ morphism.2 . obviously')
 
 restate_axiom arrow_hom.commutativity
 attribute [ematch] arrow_hom.commutativity_lemma
 
-@[extensionality] lemma ext {C : Type (u₁+1)} [large_category C] (f g : arrow C) (α β : arrow_hom f g) (w : α.morphism = β.morphism) : α = β :=
+@[extensionality] lemma ext {f g : arrow C} {α β : arrow_hom f g} (w : α.morphism = β.morphism) : α = β :=
 begin
   induction α with α_morphism,
   induction β with β_morphism,
   tidy,
 end
 
-instance Arrows (C : Type (u₁+1)) [large_category C] : large_category (arrow C):=
+instance : large_category (arrow C):=
 { hom  := arrow_hom,
   id   := by tidy,
   comp := λ X Y Z f g, ⟨ (f.morphism.1 ≫ g.morphism.1, f.morphism.2 ≫ g.morphism.2) ⟩ }
 
-@[simp] lemma Arrows_comp {C : Type (u₁+1)} [large_category C] {X Y Z : arrow C} (f : X ⟶ Y) (g : Y ⟶ Z) : f ≫ g = ⟨ (f.morphism.1 ≫ g.morphism.1, f.morphism.2 ≫ g.morphism.2) ⟩ := rfl
+@[simp] lemma arrow_comp {X Y Z : arrow C} (f : X ⟶ Y) (g : Y ⟶ Z) : f ≫ g = ⟨ (f.morphism.1 ≫ g.morphism.1, f.morphism.2 ≫ g.morphism.2) ⟩ := rfl
 
 end category_theory.arrows
 
-namespace category_theory.Functor
+namespace category_theory.functor
 
 open category_theory.arrows
 
-variable {C : Type (u₁+1)}
-variable [large_category C]
-variable {D : Type (u₂+1)}
-variable [large_category D]
+variables {C : Type u₁} [category.{u₁ v₁} C] {D : Type u₂} [category.{u₂ v₂} D]
 
-def onArrows : (C ↝ D) ↝ ((arrow C) ↝ (arrow D)) := 
+def map_arrows : (C ↝ D) ↝ ((arrow C) ↝ (arrow D)) := 
 { obj := λ F,     { obj := λ X, ⟨ (F X.1.1, F X.1.2), F.map X.2 ⟩,
                     map' := λ X Y f, ⟨ (F.map f.morphism.1, F.map f.morphism.2) ⟩ },
   map' := λ F G τ, { app := λ X, ⟨ (τ X.1.1, τ X.1.2) ⟩ } }
 
-end category_theory.Functor
+end category_theory.functor
 
