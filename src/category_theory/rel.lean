429a361ac8209aded55a802241b6e9ae061536c6
Scott Morrison <scott@tqft.net>
2018-11-19 07:16:53 +1100

    minor

--- src/category_theory/rel.lean
+++ src/category_theory/rel.lean
@@ -1,11 +1,10 @@
 import category_theory.category
-import category_theory.tactics.obviously
 
 namespace category_theory
 
 universe u
 
-def Rel : large_category (Type u) := 
+def Rel : large_category (Type u) :=
 { hom  := λ X Y, X → Y → Prop,
   id   := λ X, λ x y, x = y,
   comp := λ X Y Z f g x z, ∃ y, f x y ∧ g y z }
